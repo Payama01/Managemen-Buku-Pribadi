cd603c559cafdba79468ffab6714d24914a9c98a
R21-2000 <rakhafian9010@gmail.com>
2025-04-13 00:20:01 +0700

    commit test+ tambah data

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 const path = require('path'); // Import path module
 
 app.set('view engine', 'ejs');
-
+ 
 mongoose.connect('mongodb://localhost/ManagemenBuku')
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.error('Could not connect to MongoDB...'));
